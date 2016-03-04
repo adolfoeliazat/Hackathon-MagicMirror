ad60b567c459e57493d1df69c4fdff54b64f09dc
Valerio Donati <vdonati@newstore.com>
2016-03-04 15:21:15 +0100

    Pick variant directive

--- web/app/shared/directives.js
+++ web/app/shared/directives.js
@@ -30,4 +30,47 @@ angular.module('hq.directives', [])
             update();
         }
     };
+})
+
+.directive('sizeAndColor', function($state, $stateParams) {
+    return {
+        template: '<ul class="left">'+
+                      '<li ng-repeat="color in colors">'+
+                          '<a ng-class="{active : selectedColor===color}" ng-click="selectColor(color)">{{color}}</a>'+
+                      '</li>'+
+                  '</ul>'+
+                  '<ul ng-show="selectedColor" class="right">'+
+                      '<li ng-repeat="size in sizes">'+
+                          '<a href="{{size.url}}">{{size.size}}</a>'+
+                      '</li>'+
+                  '</ul>',
+        restrict: 'E',
+        scope: {
+            colors: '=',
+            variations: '='
+        },
+        link: function($scope, $element, $attrs) {
+            $scope.selectColor = function(color) {
+                $scope.selectedColor = color;
+                $scope.sizes = [];
+
+                $scope.variations.map(function(variation) {
+                    if (variation.variation_color_value === color) {
+                        $scope.sizes.push({
+                            size: variation.variation_size_value,
+                            url: variationUrl(variation.product_id)
+                        });
+                    }
+                });
+            }
+
+            var variationUrl = function(productId) {
+                return $state.href('magicmirror.details', {
+                    productId: productId,
+                    catalog: $stateParams.catalog,
+                    locale: $stateParams.locale
+                });
+            };
+        },
+    };
 });
